260169d39b5bc08ed24eff3a4db54935f122562c
Vinicius Zapala dos Santos <vinizap4@outlook.com>
2020-06-02 07:25:44 -0300

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; // importa o react 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css'; // importa o css
+
+function App() { // cria uma função chamada App
+  return ( // indica seu retorno
+    <div className="App"> {/* seu retorno é um elemeto html */}
+      Hello World!!!
     </div>
-  );
-}
+  ); // close 
+} // close App()
 
-export default App;
+export default App; // e exporta como padrão o App() 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'; // importa o react
+import ReactDOM from 'react-dom'; // importa o reactDom
+import './index.css'; // importa o css 
+import App from './App'; // importa o App
+import * as serviceWorker from './serviceWorker'; // importa o serviceWorker
 
-ReactDOM.render(
+ReactDOM.render( // com o reactDom ele usa o metodo render que possui doi parametros o conteudo e o lugar
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App /> {/* aqui ele colocao a função App como conteudo para renderizar */}
+  </React.StrictMode>, 
+  document.getElementById('root') // aqui ele pega o ID 'root' do index.html e rendezida dentro dele
 );
 
 // If you want your app to work offline and load faster, you can change
