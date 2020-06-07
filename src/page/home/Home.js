c2b1735979e79924fb9b05e860e43e407c4be14e
Vinicius Zapala dos Santos <vinizap4@outlook.com>
2020-06-07 01:46:15 -0300

    deploy

---
+++ src/page/home/Home.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+//import styles
+import './Home.css'
+
+//import Components
+import CreatePage from '../../Components/CreatePage/CreatePage'
+import { Title, SubTitle, Text } from '../../Components/TextContent/TextContent'
+import ArticleItem from '../../Components/ArticleItem/ArticleItem'
+
+
+export default function Home(){
+  return(
+    <CreatePage name="Home">
+      <Title style={{textDecoration:"none"}}>
+        Bem-Vindo à ConnectDevelopers
+      </Title>
+      <div className="HomeArticles">
+        <SubTitle>
+          Artigos recentes:
+        </SubTitle>
+        <div className="articlesArea">
+          <ArticleItem title="Title Test View" href="/ProgrammingLogic"> 
+            <Text> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </Text>
+          </ArticleItem>
+          <ArticleItem title="Title Test View" href="/ProgrammingLogic"> 
+            <Text> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </Text>
+          </ArticleItem>
+          <ArticleItem title="Title Test View" href="/ProgrammingLogic"> 
+            <Text> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </Text>
+          </ArticleItem>
+          <ArticleItem title="Title Test View" href="/ProgrammingLogic"> 
+            <Text> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </Text>
+          </ArticleItem>
+          <ArticleItem title="Title Test View" href="/ProgrammingLogic"> 
+            <Text> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </Text>
+          </ArticleItem>
+          <ArticleItem title="Title Test View" href="/ProgrammingLogic"> 
+            <Text> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </Text>
+          </ArticleItem>  
+        </div>
+      </div>
+    </CreatePage>
+  )
+}
--- src/router.js
+++ src/router.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter, Switch,Route } from 'react-router-dom'
 
 // import pages
-import Home from './page/home/home'
+import Home from './page/home/Home'
 import ProgrammingLogic from './page/ProgrammingLogic/ProgrammingLogic'
 
 export default function src(){
   return(
     <BrowserRouter>
       <Switch>
+        
         <Route path="/" exact component={Home} />
         <Route path="/ProgrammingLogic" exact component={ProgrammingLogic} />
       </Switch>
