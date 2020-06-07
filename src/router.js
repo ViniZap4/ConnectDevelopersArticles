e3b8877afa25e26aba0c1e168f3d9ec26adcc12d
Vinicius Zapala dos Santos <vinizap4@outlook.com>
2020-06-07 01:36:18 -0300

    deploy

--- src/router.js
+++ src/router.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Switch,Route } from 'react-router-dom'
 
 // import pages
 import Home from './page/home/home'
-
+import ProgrammingLogic from './pages/ProgrammingLogic/ProgrammingLogic'
 
 export default function src(){
   return(
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
-
+        <Route path="/ProgrammingLogic" exact component={ProgrammingLogic} />
        
       </Switch>
     </BrowserRouter>
